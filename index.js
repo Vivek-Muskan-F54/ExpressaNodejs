0e16de84efaacc42f84bc87e86f29b10549c1ac0
Vivek Muskan <vivekmuskan5847@gmail.com>
2025-10-18 13:22:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/users/:id", (req, res) => {
+  console.log(req.params);
+  res.send({
+    name: "muskann",
+    age: "20",
+    userid: "pnnitish",
+    password: "pn123456",
+  });
+});
+
+app.use("/about", (req, res) => {
+  res.send("Hello, This is backend");
+});
+
+app.use("/contact", (req, res) => {
+  res.send("This is Contact page");
+});
+
+app.use("/details", (req, res) => {
+  res.send("This is details page");
+});
+
+app.use("/", (req, res) => {
+  res.send("This is HOME page");
+});
+
+module.exports = app;
